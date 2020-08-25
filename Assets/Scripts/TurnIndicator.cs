83ff4781551cf3d138aea01bc4a49e89f018dcf0
mark-sawyer <alfador_321@hotmail.com>
2020-08-25 22:20:30 +1000

    Added turn indicator sprite

---
+++ Assets/Scripts/TurnIndicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnIndicator : MonoBehaviour {
+    public Sprite white;
+    public Sprite black;
+
+    private void Start() {
+        GameEvents.changeTurn.AddListener(changeSprite);
+    }
+
+    private void changeSprite() {
+        if (GetComponent<SpriteRenderer>().sprite == white) {
+            GetComponent<SpriteRenderer>().sprite = black;
+        }
+        else {
+            GetComponent<SpriteRenderer>().sprite = white;
+        }
+    }
+}
